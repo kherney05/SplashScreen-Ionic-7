14d74c6eca374a09b1b8d69ddc4223da9409d158
Kevin Herney Vente Angulo <kevinthebestfriends@gmail.com>
2023-07-04 16:30:36 -0500

    Splash screen terminada

---
+++ capacitor.config.ts
@@ -0,0 +1,33 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SplashScreen',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      //launchAutoHide: true,
+      //launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      //androidSplashResourceName: "splash",
+      //androidScaleType: "CENTER_CROP",
+      //showSpinner: true,
+      showSpinner: false,
+      //androidSpinnerStyle: "large",
+      androidSpinnerStyle: "small",
+      iosSpinnerStyle: "small",
+      //spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      //layoutName: "launch_screen",
+      //useDialog: true
+    },
+  },
+ 
+};
+
+export default config;
